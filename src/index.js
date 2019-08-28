bc0090930092f6bc3c6da5359fd73e0f88af5149
FromMars <inforisposta@ya.ru>
2019-08-28 22:49:44 +0300

    homework2 tasks 1-4 added

--- src/index.js
+++ src/index.js
@@ -6,17 +6,38 @@
  Напишите аналог встроенного метода forEach для работы с массивами
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
-function forEach(array, fn) {
-}
+ function forEach(array, fn) {
+     for (var i = 0; i < array.length; i++) {
+         fn(array[i], i, array);
 
+         console.log(i, array[i], array);
+     }
+ }
+ var array = ["a", "b", "c", "d"];
+ const fn = (item, i, array) => {
+   return item + 1;
+ }
+ forEach(array, fn);
 /*
  Задание 2:
 
  Напишите аналог встроенного метода map для работы с массивами
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
-function map(array, fn) {
-}
+ function map(array, fn) {
+     var array2 = [];
+     for (let i = 0; i < array.length; i++) {
+         array2[i] = fn(array[i], i, array);
+     }
+     console.log(array2);
+     return array2;
+ }
+ var array = ["a", "b", "c", "d"];
+ const fn = function(item, i, array2) {
+     return item;
+ }
+ map(array, fn);
+ console.log(array);
 
 /*
  Задание 3:
@@ -24,8 +45,25 @@ function map(array, fn) {
  Напишите аналог встроенного метода reduce для работы с массивами
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
-function reduce(array, fn, initial) {
-}
+ function reduce(array, fn, initial) {
+   let i = 0;
+   let result = initial || array[i++];
+
+   while (i < array.length) {
+     result = fn(result, array[i], i, array);
+     i++;
+   }
+   console.log(result);
+   return result;
+ }
+ var array = ["a", "b", "c", "d"];
+
+ const fn = function(result, array[i], i, array) {
+   return previousValue;
+ }
+
+ reduce(array, fn, initial)
+
 
 /*
  Задание 4:
@@ -35,9 +73,21 @@ function reduce(array, fn, initial) {
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {
-}
-
+ function upperProps(obj) {
+   var arr = [];
+   for (var item in obj) {
+     item = item.toUpperCase();
+     arr.push(item);
+   }
+   console.log(arr);
+   return arr;
+ }
+ var obj = {
+   name: 'man',
+   eyesColor: 'brown',
+   hairColor: 'black'
+ };
+ upperProps(obj)
 /*
  Задание 5 *:
 
