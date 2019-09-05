cb85a274b180ed8d844a1664ce4940ee374cc1e7
FromMars <inforisposta@ya.ru>
2019-09-05 23:09:38 +0300

    homework 5 tasks 1-3 fixed

--- src/index.js
+++ src/index.js
@@ -9,10 +9,7 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
-  target.addEventListener(eventName, fn(event) {
-    //....
-    }););
-
+  target.addEventListener(eventName, fn(event));
 }
 
 /*
@@ -36,8 +33,8 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
-  target.addEventListener(eventName, function (eventName) {
-    eventName.preventDefault();
+  target.addEventListener(eventName, fn (eventName) {
+    eventName.preventDefault();)
 }
 
 /*
@@ -62,6 +59,7 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+
 }
 
 /*
@@ -74,8 +72,8 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  target.addEventListener('click', function (e) {
-    if (e.target.tagName == "BUTTON") {
+  target.addEventListener('click', function (f) {
+    if (f.target.tagName == "BUTTON") {
         fn();
         target.removeEventListener(eventName, fn);
     }
